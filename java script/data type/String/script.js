e3b044b6eaccb7183b84a59eba35a13c2ba4a6c0
Avee hacker <153273657+avee786@users.noreply.github.com>
2025-12-31 10:53:49 +0530

    Add files via upload

---
+++ java script/data type/String/script.js
@@ -0,0 +1,7 @@
+function showString() {
+    let text = "Hello Students";
+
+    document.getElementById("output").innerHTML =
+        "Value: " + text + "<br>" +
+        "Data Type: " + typeof text;
+}
---
+++ java script/java.js
@@ -0,0 +1,6 @@
+let text = "hello";  //string
+let number = 10 ;   //Numric
+let istrue = true;   //boolean
+let x = null;    //Null
+let obj = {name:'avee'}  //object
+let arr = [1,2,3];   //array
